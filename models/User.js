4f8f576e3dfa12eda4d4fe1c0a2b2b2ae3305b13
VijayaragavanS1999 <sasigaviprt@gmail.com>
2026-04-02 22:14:08 +0530

    feat: add user model with schema and validation

---
+++ models/User.js
@@ -0,0 +1,76 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+  },
+  name: {
+    type: String,
+    trim: true,
+  },
+  // Freshdesk credentials
+  freshdeskApiKey: {
+    type: String,
+    default: null,
+  },
+  freshdeskDomain: {
+    type: String,
+    default: null,
+  },
+  // HubSpot OAuth tokens
+  hubspotAccessToken: {
+    type: String,
+    default: null,
+  },
+  hubspotRefreshToken: {
+    type: String,
+    default: null,
+  },
+  hubspotTokenExpiry: {
+    type: Date,
+    default: null,
+  },
+}, { timestamps: true });
+
+// Hash password before save
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 12);
+  next();
+});
+
+// Compare password
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  return bcrypt.compare(candidatePassword, this.password);
+};
+
+// Virtual: integration status
+userSchema.virtual('integrations').get(function () {
+  return {
+    freshdesk: !!(this.freshdeskApiKey && this.freshdeskDomain),
+    hubspot: !!(this.hubspotAccessToken),
+  };
+});
+
+userSchema.set('toJSON', {
+  virtuals: true,
+  transform: (doc, ret) => {
+    delete ret.password;
+    delete ret.freshdeskApiKey;
+    delete ret.hubspotAccessToken;
+    delete ret.hubspotRefreshToken;
+    return ret;
+  },
+});
+
+module.exports = mongoose.model('User', userSchema);
